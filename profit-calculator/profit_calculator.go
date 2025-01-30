894074375de80d2708b4f4e549a817c635091d3e
luisvinicius403 <luisvinicius.dev@gmail.com>
2025-01-29 21:52:07 -0300

    refactor(profit-calculator): modularize calcs and user input handling

--- profit-calculator/profit_calculator.go
+++ profit-calculator/profit_calculator.go
@@ -3,22 +3,34 @@ package main
 import "fmt"
 
 func main() {
-	var revenue, expenses, taxRate float32
+	revenue := getUserInput("What is your company`s Revenue? ")
+	expense := getUserInput("What is your company`s Expenses? ")
+	taxRate := getUserInput("What is your company`s Tax Rate? ")
 
-	fmt.Print("What is your company`s Revenue? ")
-	fmt.Scan(&revenue)
-
-	fmt.Print("What is your company`s Expenses? ")
-	fmt.Scan(&expenses)
-
-	fmt.Print("What is your company`s Tax Rate? ")
-	fmt.Scan(&taxRate)
-
-	earningsBeforeTax := revenue - expenses
-	profit := earningsBeforeTax * (1 - taxRate/100)
-	ratio := earningsBeforeTax / profit
+	earningsBeforeTax := calcEarningsBeforeTax(revenue, expense)
+	profit := calcProfit(earningsBeforeTax, taxRate)
+	ratio := calcRatio(earningsBeforeTax, profit)
 
 	fmt.Printf("\nEarnings Before Tax: $%.2f", earningsBeforeTax)
 	fmt.Printf("\nProfit: $%.2f", profit)
 	fmt.Printf("\nRatio: %.2f", ratio)
+}
+
+func getUserInput(question string) float32 {
+	var userInput float32
+	fmt.Print(question)
+	fmt.Scan(&userInput)
+	return userInput
+}
+
+func calcEarningsBeforeTax(revenue, expense float32) float32 {
+	return revenue - expense
+}
+
+func calcProfit(ebt, taxRate float32) float32 {
+	return ebt * (1 - taxRate/100)
+}
+
+func calcRatio(ebt, profit float32) float32 {
+	return ebt / profit
 }
