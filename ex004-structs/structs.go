52d9c8e8d4396c38bc2e2ff62b1181a427d6353f
luisvinicius403 <luisvinicius.dev@gmail.com>
2025-02-08 14:59:11 -0300

    feat(structs): add clearUsername and newUser methods

--- ex004-structs/structs.go
+++ ex004-structs/structs.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"time"
 )
@@ -12,33 +13,53 @@ type user struct {
 	createAt  time.Time
 }
 
+func (user user) outputUserDatails() {
+	fmt.Println(user.firstName)
+	fmt.Println(user.lastName)
+	fmt.Println(user.birthDate)
+}
+
+func (user *user) clearUsername() {
+	user.firstName = ""
+	user.lastName = ""
+}
+
+func newUser(firstName, lastName, birthDate string) (*user, error) {
+	if firstName == "" || lastName == "" || birthDate == "" {
+		return nil, errors.New("first name, last name or birthdate are required")
+	}
+
+	return &user{
+		firstName: firstName,
+		lastName:  lastName,
+		birthDate: birthDate,
+		createAt:  time.Now(),
+	}, nil
+}
+
 func main() {
 	userFirstName := getUserInput("Please enter your first name: ")
 	userLastName := getUserInput("Please enter your last name: ")
 	UserBirthDate := getUserInput("Please enter your birthdate (MM/DD/YYYY): ")
 
-	var appUser user
+	var appUser *user
 
-	appUser = user{
-		firstName: userFirstName,
-		lastName:  userLastName,
-		birthDate: UserBirthDate,
-		createAt:  time.Now(),
+	appUser, err := newUser(userFirstName, userLastName, UserBirthDate)
+
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
 
-	outputUserDatails(appUser)
+	appUser.outputUserDatails()
+	appUser.clearUsername()
+	appUser.outputUserDatails()
 }
 
 func getUserInput(question string) string {
 	var userInput string
 
 	fmt.Print(question)
-	fmt.Scan(&userInput)
+	fmt.Scanln(&userInput)
 	return userInput
-}
-
-func outputUserDatails(user user) {
-	fmt.Println(user.firstName)
-	fmt.Println(user.lastName)
-	fmt.Println(user.birthDate)
 }
